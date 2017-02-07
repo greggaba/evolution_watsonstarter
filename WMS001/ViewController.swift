2ad9a8a3384dc9a2fa7e10ab2243aa33c0592b4b
Tanmay Bakshi <tanmaybakshi@Tanmays-MacBook-Pro.local>
2017-02-07 11:23:58 -0500

    Initial Commit

---
+++ WMS001/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WMS001
+//
+//  Created by Tanmay Bakshi on 2017-01-16.
+//  Copyright © 2017 Tanmay Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
